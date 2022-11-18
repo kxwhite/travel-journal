cb06b823b1279ea78c9b28bc83d1a544e5022785
Keon White <keon-1@hotmail.co.uk>
2022-11-18 10:59:51 +0000

    Change Japan image URL

--- src/data.jsx
+++ src/data.jsx
@@ -11,7 +11,7 @@ export default [
     startDate: "12 Jan, 2021",
     endDate: "24 Jan, 2021",
     description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+    imageUrl: "https://images.unsplash.com/photo-1547823065-4cbbb2d4d185?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80",
   },
   {
     id: 2,
